a9ba5a20525fbaf8e875afca34d8f43399993ce9
sushanthreddych <139258151+sushanthreddych@users.noreply.github.com>
2026-01-01 11:38:31 -0500

    Refactor condition and echo statement in pipeline

--- TestPipeline.groovy
+++ TestPipeline.groovy
@@ -4,12 +4,16 @@ pipeline {
     stages {
         stage('Hello') {
             when {
-                env.GIT_BRANCH == 'origin/main' || env.GIT_BRANCH == 'main'
+                expression {
+                    env.GIT_BRANCH == 'origin/main' || env.GIT_BRANCH == 'main'
+                }
             }
             steps {
-                echo 'Hello World ${env.GIT_BRANCH'}
+                echo "Hello World ${env.GIT_BRANCH}"
             }
         }
+    }
+
     post {
         success {
             echo 'Pipeline completed'
